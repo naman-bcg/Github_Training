a4d8caed3e08b19f8d3bd7ce6469292a93132402
naman-bcg <naman-bcg@users.noreply.github.com>
2018-09-17 11:11:21 +0530

    Commiting first code
    Adding first code

---
+++ first.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep 11 21:49:22 2018
+
+@author: Gupta2 Abhishek
+"""
+
+#importing the libraries
+import os
+import sys
+
+print(sys.version)
+print("My first python code")
+
+#calculating sum of two numbers
+x=2
+y=4
+
+print("Sum of two numbers is {}".format(x+y))
